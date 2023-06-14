4c7ebbbb4d87bf6f5a21171d901fb3fb686344ac
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2023-06-14 08:42:37 -0300

    update

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import time
 import numpy as np
 import pygame
 
-TILE = 15
-RES = WIDTH, HEIGTH = 1600, 900
+TILE = 12
+RES = WIDTH, HEIGTH = 1000, 600
 W, H = WIDTH//TILE, HEIGTH//TILE
+
 COLOR_BG = (10, 10, 10)
 COLOR_GRID = (40, 40, 40)
 COLOR_DIE_NEXT = (170, 170, 170)
 COLOR_ALIVE_NEXT = (255, 255, 255)
 
-INFINIT = [[40, 18], [40, 19], [41, 18], [41, 19], [53, 15], [52, 16], [54, 16], [51, 17], [55, 17], [56, 17], [51, 18], [51, 19], [52, 20], [55, 18], [55, 19], [54, 20], [56, 20], [53, 21], [57, 18], [57, 19], [57, 20], [58, 18], [58, 19], [
-    58, 21], [59, 19], [59, 21], [60, 20], [63, 17], [63, 18], [63, 22], [63, 23], [64, 17], [64, 19], [64, 21], [64, 23], [65, 18], [65, 19], [65, 20], [65, 21], [65, 22], [66, 19], [66, 20], [66, 21], [67, 20], [74, 20], [74, 21], [75, 20], [75, 21]]
+INFINIT = [
+    [40, 18], [40, 19], [41, 18], [41, 19], [53, 15], [52, 16], [54, 16], [51, 17], [55, 17], [56, 17], [51, 18], [51, 19], [52, 20], [55, 18], [55, 19], [54, 20], [56, 20], [53, 21], [57, 18], [57, 19], [57, 20], [58, 18], [58, 19], [
+        58, 21], [59, 19], [59, 21], [60, 20], [63, 17], [63, 18], [63, 22], [63, 23], [64, 17], [64, 19], [64, 21], [64, 23], [65, 18], [65, 19], [65, 20], [65, 21], [65, 22], [66, 19], [66, 20], [66, 21], [67, 20], [74, 20], [74, 21], [75, 20], [75, 21]
+]
 
 
 def update(screen, cells, size, with_progress=False):
@@ -45,9 +48,10 @@ def main():
     screen = pygame.display.set_mode(RES)
 
     # cells = np.zeros((H, W))
-    cells = np.array([[1 if not (i*j) % 17 else 0 for i in range(W)]
-                     for j in range(H)])
-    # cells = np.array([[1 if [i, j] in INFINIT else 0 for i in range(W)]for j in range(H)])
+    # cells = np.array([[1 if not (i*j) % 33 else 0 for i in range(W)]
+    #                   for j in range(H)])
+    cells = np.array(
+        [[1 if [i, j] in INFINIT else 0 for i in range(W)]for j in range(H)])
 
     screen.fill(COLOR_GRID)
     update(screen, cells, TILE)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-map(lambda x, y: [y, x + 20], a))
+    cells = np.array([[1 if not (i*j) % 33 else 0 for i in range(W)]
+                      for j in range(H)])
